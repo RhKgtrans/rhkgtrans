e24c90b2f9266724678762ac266882bb8c74d62f
RhKgtrans <92178410+RhKgtrans@users.noreply.github.com>
2021-10-12 02:36:44 -0700

    Update and rename config.sample.php to config.php

---
+++ app/config.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('error_log', '_LOG_');
+
+define('APP_NAME', 'Ice Hrm');
+define('FB_URL', 'Ice Hrm');
+define('TWITTER_URL', 'Ice Hrm');
+
+define('CLIENT_NAME', 'app');
+define('APP_BASE_PATH', 'rhkgtrans/');
+define('CLIENT_BASE_PATH', 'rhkgtrans/app/');
+define('BASE_URL','http://rh-kgtrans.herokuapp.com/');
+define('CLIENT_BASE_URL','http://rh-kgtrans.herokuapp.com/app/');
+
+define('APP_DB', 'sql11443040');
+define('APP_USERNAME', 'sql11443040');
+define('APP_PASSWORD', 'iIxXpD8m2a');
+define('APP_HOST', 'sql11.freemysqlhosting.net');
+define('APP_CON_STR', 'mysqli://'.APP_USERNAME.':'.APP_PASSWORD.'@'.APP_HOST.'/'.APP_DB);
+
+//file upload
+define('FILE_TYPES', 'jpg,png,jpeg');
+define('MAX_FILE_SIZE_KB', 10 * 1024);
